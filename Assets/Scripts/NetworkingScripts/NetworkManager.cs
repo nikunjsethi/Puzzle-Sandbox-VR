98b8a8713255785c91efa7d091267163c24e1d8d
nikunj2706 <nikunjsth5@gmail.com>
2023-02-07 11:06:28 +0000

    photon multiplayer setup , voice integration setup, both working

---
+++ Assets/Scripts/NetworkingScripts/NetworkManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        ConnectToServer();
+    }
+
+    void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try connect to Server....");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Server.");
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 10;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a Room");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("A new player joined the room.");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
