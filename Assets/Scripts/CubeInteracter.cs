79fddaf1f92a05c0e601dd46682d0d008fd1ff76
nikunj2706 <nikunjsth5@gmail.com>
2023-03-14 13:19:23 +0000

    .

--- Assets/Scripts/CubeInteracter.cs
+++ Assets/Scripts/CubeInteracter.cs
@@ -5,12 +5,28 @@ using Photon.Pun;
 using Photon.Realtime;
 public class CubeInteracter : MonoBehaviour
 {
+    public PhotonView pv;
+
+    private void Start()
+    {
+        pv = GetComponent<PhotonView>();
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Interactable"))
+        if(pv.IsMine)
         {
-            PhotonNetwork.Destroy(other.gameObject);
-            gameObject.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = true;
+            if (other.CompareTag("Interactable"))
+            {
+                PhotonNetwork.Destroy(other.gameObject);
+                pv.RPC("RPC_ColorChanger",RpcTarget.AllBuffered);
+            }
         }
     }
+
+    [PunRPC]
+    void RPC_ColorChanger()
+    {
+        gameObject.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = true;
+    }
+
 }
