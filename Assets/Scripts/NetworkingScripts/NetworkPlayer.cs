614fa43f59eac4962bac70fe2882f82850a7f2f5
nikunj2706 <nikunjsth5@gmail.com>
2023-03-14 12:17:54 +0000

    debug

--- Assets/Scripts/NetworkingScripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkingScripts/NetworkPlayer.cs
@@ -37,7 +37,7 @@ public class NetworkPlayer : MonoBehaviour
             Debug.Log("Player count is : " + playerCount);
             origin.transform.position = instantiationPoint[playerCount].transform.position;
             origin.transform.rotation = instantiationPoint[playerCount].transform.rotation;
-            pv.RPC("RPC_Array_Update", RpcTarget.AllBuffered);
+           
         }
     }
 
@@ -62,18 +62,11 @@ public class NetworkPlayer : MonoBehaviour
             MappingMovement(Head, headRig);
             MappingMovement(LeftHand, leftHandRig);
             MappingMovement(RightHand, rightHandRig);
+            pv.RPC("RPC_Array_Update", RpcTarget.AllBuffered);
         }
 
     }
 
-    [PunRPC]
-    void RPC_PlayerCount()
-    {
-        if(PhotonNetwork.IsMasterClient)
-        {
-
-        }
-    }
     void MappingMovement(Transform target, Transform rigTransform)
     {
         target.position = rigTransform.position;
